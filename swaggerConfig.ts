30b921c05281b91f707b489cd4bca3a29fbf2acc
Tejas013 <tejasmadhukar6@gmail.com>
2024-07-26 02:47:36 +0530

    Updated swaggerUI description

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -7,7 +7,8 @@ const options: Options = {
 		info: {
 			title: 'Project and Report API',
 			version: '1.0.0',
-			description: 'API for managing projects and reports',
+			description:
+				'API for managing projects and reports\nFor endpoints to work you need Authorize with the password: Password123.',
 		},
 	},
 	apis: ['./src/controllers/*.ts', './src/models/*.ts'],
